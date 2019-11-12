97ba5ddd5f17b5d1d1034e9eb6e9d51857d40332
or abramov <or.a@bidalgo.com>
2019-11-12 23:08:30 +0200

    add high and low to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('unable to find location. Try another search', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' it is currently ' + body.currently.temperature + ' degree out. there is a ' + body.currently.precipProbability + '% chance of rain'
+            callback(undefined, body.daily.data[0].summary + ' it is currently ' + body.currently.temperature + ' degree out. The high today is ' + body.daily.data[0].temperatureHigh + ' with a low of ' + body.daily.data[0].temperatureLow + '. There is a ' + body.currently.precipProbability + '% chance of rain'
             )
         }
     })
